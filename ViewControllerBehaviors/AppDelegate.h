a39a8bf62d261f4499205c6c52a5a608bab06dc9
Ilya Dudarenko <IODudarenko.SBT@sberbank.ru>
2017-02-06 17:11:13 +0300

    initial commit

---
+++ ViewControllerBehaviors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewControllerBehaviors
+//
+//  Created by Dudarenko Ilya on 06.02.17.
+//  Copyright © 2017 DIO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewControllerBehaviors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewControllerBehaviors
+//
+//  Created by Dudarenko Ilya on 06.02.17.
+//  Copyright © 2017 DIO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
